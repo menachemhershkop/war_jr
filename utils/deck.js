d5ba8df23429cb71c9b0081e90195ebc3226a4f7
yosef morad <yos0556773004@gmail.com>
2025-12-07 12:12:15 +0200

    updatre

---
+++ utils/deck.js
@@ -0,0 +1,38 @@
+export function createCard(rank, suite){
+  const cards = {"2":2,"3":3,"4":4,"5":5,"6":6,"7":7,"8":8,"9":9,"10":10,"J":11,"Q":12,"K":13,"A":14}
+//   const type = ["H","C","D","S"]
+  if (rank in cards){
+    return {"rank":rank,"suite":suite,"value":cards[rank]}
+  }
+}
+
+ export function compareCards(p1Cards,p2Cards){
+    if (p1Cards["value"]> p2Cards["value"]){
+        return "p1 is won"
+    }
+    else if (p1Cards["value"]< p2Cards["value"]){
+        return "p2 is won"
+    }
+    else {
+        return "War!!!"
+    }
+ }
+ 
+export function createDeck(){
+    const cards = [2,3,4,5,6,7,8,9,10,"J","Q","K","A"]
+    const suit = ["H", "C", "D", "S"]
+    let fullCards = []
+
+    for (let i=0; i< suit.length; i++){
+        
+        
+        for (let j=0; j< cards.length; j++){
+            
+            
+           fullCards.push({suit:suit[i],card:cards[j]}) 
+        }
+    }
+    return fullCards
+}
+
+
